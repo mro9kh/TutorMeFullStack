e2ac094822794afeb3a98b87bf60f2f82dccfc20
DamasentheGiant <davidlee.2001@hotmail.com>
2023-02-25 19:55:48 -0500

    added models for different users

--- oauth_app/models.py
+++ oauth_app/models.py
@@ -1,1 +1,28 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+# Create your models here.
+
+# allows creation of different users
+# class User(AbstractUser):
+#     class Role(models.TextChoices):
+#         TUTOR = "tutor"
+#         STUDENT = "student"
+
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+
+
+# tutor model, only holds username for now
+class Tutor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.TextField()  # can add computing id
+
+
+# Student model
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.TextField()  # can add computing id
