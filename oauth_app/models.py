b25cc8f667e5dccd94302b1cf76ef54fe67fca90
Meg Owens <megowens@Megs-MacBook-Air.local>
2023-03-14 19:54:52 -0400

    profile display: username

--- oauth_app/models.py
+++ oauth_app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_tutor = models.BooleanField(default=False)
-#    course_list = 
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
