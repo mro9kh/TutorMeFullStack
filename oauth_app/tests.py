5d52705227ea0d9e37080f2e74cafc74ebdf51a9
Himayatullah Azizi <hja8ybr@virginia.edu>
2023-03-18 16:28:28 -0400

    test student model failing

--- oauth_app/tests.py
+++ oauth_app/tests.py
@@ -1,14 +1,19 @@
 from django.test import TestCase
-from oauth_app.models import User
+from .models import User, Student, Tutor
 
 class UserTestCase(TestCase):
 
     def test_identifies_title(self):
-        ya = User.objects.create(username= "Henrry", is_student = False, is_tutor=True)
-        yeet = User.objects.create(username= "Jason", is_student = True, is_tutor=False)
-        print(ya)
-        print(yeet)
-        self.assertEqual(ya.is_tutor, True)
-        self.assertEqual(yeet.is_student, True)
+        testTutor = User.objects.create(username= "Henrry", is_student = False, is_tutor=True)
+        testStudent = User.objects.create(username= "Jason", is_student = True, is_tutor=False)
+        print(testTutor)
+        print(testStudent)
+        self.assertEqual(testTutor.is_tutor, True)
+        self.assertEqual(testStudent.is_student, True)
 
 
+    def test_student_model(self):
+        testStudent = User.objects.create(username="Jason", is_student= True, is_tutor = False)
+        test_model = Student.objects.create(user=testStudent, name = "two", year = 3)
+        print(test_model = Student.objects.create(user=testStudent, name = "two", year = 3))
+        self.assertEqual(test_model.year, 3)
