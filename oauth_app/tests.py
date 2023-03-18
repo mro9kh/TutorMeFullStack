651216cff1b9223bf5c89c2a008b8d4396520396
Himayatullah Azizi <hja8ybr@virginia.edu>
2023-03-18 16:15:55 -0400

    test correct title works

--- oauth_app/tests.py
+++ oauth_app/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
-from .models import User, Tutor, Student
+from oauth_app.models import User
 
 class UserTestCase(TestCase):
-    # def setUp(self):
-    #     User.objects.create(is_student= False, is_tutor= True)
-    #     User.objects.create(is_student= True, is_tutor= False)
+
     def test_identifies_title(self):
-        student1 = User.objects.create(is_student= False, is_tutor= True)
-        student2 = User.objects.create(is_student= True, is_tutor= False)
-        self.assertEqual(student1.is_tutor, True)
-        self.assertEqual(student2.is_tutor, False)
+        ya = User.objects.create(username= "Henrry", is_student = False, is_tutor=True)
+        yeet = User.objects.create(username= "Jason", is_student = True, is_tutor=False)
+        print(ya)
+        print(yeet)
+        self.assertEqual(ya.is_tutor, True)
+        self.assertEqual(yeet.is_student, True)
+
+
