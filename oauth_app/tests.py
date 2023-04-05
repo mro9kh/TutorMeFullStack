c01447c55243970f763fdbb3adae75963c4a1389
Himayatullah Azizi <hja8ybr@virginia.edu>
2023-04-05 17:04:17 -0400

    Test if user both student and tutor

--- oauth_app/tests.py
+++ oauth_app/tests.py
@@ -10,6 +10,14 @@ class UserTestCase(TestCase):
        randoUser = User.objects.create(username="testTutor", is_tutor = True, is_student= False)
        self.assertEqual(randoUser.is_tutor, True)
 
+   def test_user_both_isStudent(self):
+       randoUser = User.objects.create(username="testStudent", is_tutor = True, is_student= True)
+       self.assertEqual(randoUser.is_student, True)
+
+   def test_user_both_isTutor(self):
+       randoUser = User.objects.create(username="testTutor", is_tutor = True, is_student= True)
+       self.assertEqual(randoUser.is_tutor, True)
+
    def test_student_model(self):
        randoUser = User.objects.create(username="testStudent", is_tutor = False, is_student= True)
        test_model = Student.objects.create(user=randoUser, name = "two", year = 3)
@@ -19,3 +27,5 @@ class UserTestCase(TestCase):
        randoUser = User.objects.create(username="testTutor", is_tutor = True, is_student= False)
        test_model = Tutor.objects.create(user=randoUser, name = "two", year = 3)
        self.assertEqual(test_model.year, 3)
+
+    
