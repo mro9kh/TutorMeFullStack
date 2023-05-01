b5efb53b9fc1c30c1a0171717731eefcd9f09b4a
coletted1 <44319285+colettedcosta@users.noreply.github.com>
2023-04-30 22:17:27 -0400

    moved everything to template

--- mysite/settings.py
+++ mysite/settings.py
@@ -129,9 +129,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# added this
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -157,7 +160,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 AUTH_USER_MODEL = 'oauth_app.User'
 
-SITE_ID = 9
+SITE_ID = 11
 
 
 SOCIALACCOUNT_QUERY_EMAIL = True
